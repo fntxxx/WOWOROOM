a12c1eb64991ee92c16749f3bff907db6523677a
Shang-Lun <fntxxx.gmail.com>
2025-12-17 11:16:02 +0800

    feat: add renderProducts function

--- script.js
+++ script.js
@@ -7,8 +7,30 @@ const initProducts = () => {
     axios.get(`${API_URL}/products`)
         .then(response => {
             products = response.data?.products ?? [];
+            renderProducts(products);
         })
         .catch(error => {
             console.error("資料載入錯誤： " + error);
         })
+};
+
+initProducts();
+
+const productWrap = document.querySelector(".productWrap");
+
+const renderProducts = (products) => {
+    const productsHtml = products
+        .map(product => `
+            <li class="productCard">
+                <h4 class="productType">新品</h4>
+                <img src="${product.images}"
+                    alt="${product.title}">
+                <a href="#" class="addCartBtn" data-id="${product.id}">加入購物車</a>
+                <h3>${product.title}</h3>
+                <del class="originPrice">NT$${product.origin_price.toLocaleString("zh-TW")}</del>
+                <p class="nowPrice">NT$${product.price.toLocaleString("zh-TW")}</p>
+            </li>
+        `).join("");
+
+    productWrap.innerHTML = productsHtml;
 };
