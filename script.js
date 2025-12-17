caeb1feb08c281237f67454883295c7e3ecab54f
Shang-Lun <fntxxx.gmail.com>
2025-12-17 11:11:51 +0800

    feat: add initProducts function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const API_PATH = "shanglun";
 const API_URL = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${API_PATH}`;
 
+let products = [];
+
+const initProducts = () => {
+    axios.get(`${API_URL}/products`)
+        .then(response => {
+            products = response.data?.products ?? [];
+        })
+        .catch(error => {
+            console.error("資料載入錯誤： " + error);
+        })
+};
