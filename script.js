b1b0e56f1660d928a5107f380a674ad6b7b11a00
Shang-Lun <fntxxx.gmail.com>
2025-12-17 11:32:10 +0800

    feat: add renderCarts function

--- script.js
+++ script.js
@@ -41,8 +41,46 @@ const initCarts = () => {
     axios.get(`${API_URL}/carts`)
         .then(response => {
             cartsInfo = response.data ?? {};
+            renderCarts(cartsInfo);
         })
         .catch(error => {
             console.error("資料載入錯誤： " + error);
         })
+};
+
+initCarts();
+
+const shoppingCartTbody = document.querySelector(".shoppingCart-table tbody");
+const shoppingCartTfoot = document.querySelector(".shoppingCart-table tfoot");
+const finalTotalSpan = document.querySelector(".shoppingCart-table tfoot td:last-of-type");
+
+const renderCarts = (cartsInfo) => {
+    const { carts = [], finalTotal = 0 } = cartsInfo;
+    const tbodyHtml = carts
+        .map(cart => {
+            const product = cart.product;
+            return `
+                <tr>
+                    <td>
+                        <div class="cardItem-title">
+                            <img src="${product.images}" alt="">
+                            <p>${product.title}</p>
+                        </div>
+                    </td>
+                    <td>NT$${product.price.toLocaleString("zh-TW")}</td>
+                    <td>${cart.quantity}</td>
+                    <td>NT$${(product.price * cart.quantity).toLocaleString("zh-TW")}</td>
+                    <td class="discardBtn">
+                        <a href="#" class="material-icons" data-id="${cart.id}">
+                            clear
+                        </a>
+                    </td>
+                </tr>
+            `;
+        }).join("");
+
+    shoppingCartTbody.innerHTML = tbodyHtml
+        ? tbodyHtml
+        : "<tr><td colspan='5' align='center'>無商品資料</td></tr>";
+    finalTotalSpan.textContent = `NT$${finalTotal.toLocaleString("zh-TW")}`;
 };
